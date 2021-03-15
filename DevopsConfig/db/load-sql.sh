25483622e7a82541cff233e38b9bbd8e18493b8f
brian le <brian.le@aibidia.com>
2021-03-15 21:31:28 +0200

    Update load-sql.sh

--- DevopsConfig/db/load-sql.sh
+++ DevopsConfig/db/load-sql.sh
@@ -8,11 +8,5 @@ do
 done
 echo "Start dump to db"
 # Dump sql file
-if [ "$MIGRATE_NEW_DB" -eq 1 ]; then
-  psql -U "$POSTGRES_USER" "$POSTGRES_DB" < $sql_file
-  echo "Dump db completed!"
-else
-  echo "No Dump data!"
-fi
-
-exit 0
+psql -U "$POSTGRES_USER" "$POSTGRES_DB" < $sql_file
+echo "Dump db completed!"
